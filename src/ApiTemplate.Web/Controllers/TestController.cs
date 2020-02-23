8a363f6b7fecd5a5873d5b89e4e42fb134d07490
FlareLine <xom.flareline@gmail.com>
2020-02-23 16:56:58 +1100

    Added test controller

---
+++ src/ApiTemplate.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTemplate.Web.Controllers
+{
+	/// <summary>
+	/// Test controller for application.
+	/// </summary>
+	[ApiController]
+	[Route("[controller]")]
+	public class TestController
+	{
+		[HttpGet]
+		public IActionResult ServerError()
+		{
+			throw new ApplicationException("This endpoint should throw a 500 server error.");
+		}
+	}
+}
